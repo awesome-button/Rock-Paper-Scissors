a7c698a53556a97143ceea5e27cb79af69072ade
lena <oleshko.lena@gmail.com>
2020-01-16 09:54:04 +0100

    Added a score panel and a function allowing to play multiple rounds until one of the players scores 5

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let buttons = document.getElementsByClassName("button");
 let playerInput = "";
 let computerInput = "";
+let a = document.getElementById("playerScore");
+let b = document.getElementById("computerScore");
+let playerScore = 0;
+let computerScore = 0;
 
 const computerSelection = () => {
     const options = ["rock", "paper", "scissors"];
@@ -10,37 +14,62 @@ const computerSelection = () => {
 
 const playRound = () => {
     computerInput = computerSelection();
-
-    if (playerInput === computerInput) return draw();
     
-    if (playerInput === "rock") {
-        (computerInput === "paper") ? lost() : won();
-    }
+        if (playerInput === computerInput) return draw();
+        
+        if (playerInput === "rock") {
+            (computerInput === "paper") ? lost() : won();
+        }
 
-    if (playerInput === "paper") {
-        (computerInput === "scissors") ? lost() : won();
-    }
+        if (playerInput === "paper") {
+            (computerInput === "scissors") ? lost() : won();
+        }
 
-    if (playerInput === "scissors") {
-        (computerInput === "rock") ? lost() : won();
-    }
+        if (playerInput === "scissors") {
+            (computerInput === "rock") ? lost() : won();
+        }
+
 }
 
 const lost = () => {
     alert(`You lost, ${computerInput} beats ${playerInput}`);
+    computerScore++;
 }
 
 const won = () => {
     alert(`You won, ${playerInput} beats ${computerInput}`);
+    playerScore++;
 }
 
 const draw = () => {
     alert(`It's a draw`);
+    computerScore++;
+    playerScore++;
 }
 
-for (let button of buttons) {
-    button.addEventListener('click', () => {
-        playerInput = button.textContent;
-        playRound();
-});
+const gameOver = () => {
+    return playerScore >= 5 || computerScore >= 5;
 }
+
+const game = () => {
+    
+    for (let button of buttons) {
+        button.addEventListener('click', () => {
+            playerInput = button.textContent;
+            playRound();
+            a.textContent = playerScore;
+            b.textContent = computerScore;
+            console.log(computerScore, playerScore);
+            if (gameOver()) {
+                alert(`Game over`);
+                playerScore = 0;
+                computerScore = 0;
+                return;
+            };
+        });
+    
+    }
+
+}
+
+game();
